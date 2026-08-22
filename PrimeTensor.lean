8bae8b306a973761287568ac27eac3cc62d503c0
Sean Evans <sean.w.evans@gmail.com>
2026-08-22 12:25:24 -0400

    encode physical H3 snapshots as weighted spectral states

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -209,3 +209,5 @@ import PrimeTensor.Tensor.Calculus
 import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.FourierCompatibility
+
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.SpectralEncoder
