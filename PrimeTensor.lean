60b87c27f8562eee736ade7c53f57af1881338d8
Sean Evans <sean.w.evans@gmail.com>
2026-08-22 13:03:18 -0400

    prove weighted H3 spectral states deweight to Fourier L1

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -211,3 +211,4 @@ import PrimeTensor.Tensor.Basic
 import PrimeTensor.Tensor.Calculus
 import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.SpectralL1
