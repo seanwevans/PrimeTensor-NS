aec5bae61d659a1734d125897fe56a0ecd42f614
Sean Evans <sean.w.evans@gmail.com>
2026-08-23 13:15:46 -0400

    close H3 spectral vorticity flux

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -244,3 +244,5 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.WeightedCon
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.WeightedConvolutionMajorantRepresentatives
 
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.WeightedConvolutionL2
+
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.VorticityFlux
