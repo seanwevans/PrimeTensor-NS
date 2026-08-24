abbdb0ca2a5eed6e978f1d15b515d92792dcc972
Sean Evans <sean.w.evans@gmail.com>
2026-08-24 19:19:27 -0400

    close H3 Fin heat Leray restart radius

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -274,3 +274,4 @@ import PrimeTensor.Tensor.Basic
 import PrimeTensor.Tensor.Calculus
 import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.FinHeatLerayRestartRadius
