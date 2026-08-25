a95f465c8716bd03ca6a1e641efb7056ac1cc44d
Sean Evans <sean.w.evans@gmail.com>
2026-08-24 23:48:23 -0400

    close H3 Schwartz spectral outer product physical closure

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -294,3 +294,4 @@ import PrimeTensor.Tensor.Basic
 import PrimeTensor.Tensor.Calculus
 import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.SchwartzSpectralOuterProductPhysicalClosure
