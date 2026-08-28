72f2bc8ba50e68aab9e0acbbcc8c3d6d98806162
Sean Evans <sean.w.evans@gmail.com>
2026-08-27 20:32:04 -0400

    close H3 Schwartz nine-quarter frequency split

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -457,3 +457,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Duhamel.Tail.Moment.FullNineQuarter
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Duhamel.Tail.Moment.MildNineQuarter
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Duhamel.Tail.Moment.MildRawNineQuarter
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Nonlinear.Forcing.Moment.NineQuarterFrequencySplit
