0a9090793e4b874f7a5cbcff7ce90755e1fd61b7
Sean Evans <sean.w.evans@gmail.com>
2026-08-30 15:58:13 -0400

    identify H3 classical overlap with selected restart

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -622,3 +622,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailOverlapGluing
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailFrontier
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailPointwiseOverlap
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailClassicalOverlap
