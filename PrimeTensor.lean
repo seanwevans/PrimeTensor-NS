1777843df12535eed74238c55e624e3e4cbefe30
Sean Evans <sean.w.evans@gmail.com>
2026-08-30 16:34:15 -0400

    decompose unit viscosity H3 local PDE frontier

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -626,3 +626,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailGlueSpatialRegularity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailGlueThirdJetContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailUnitViscosityFrontier
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.UnitViscosityPDEDecomposition
