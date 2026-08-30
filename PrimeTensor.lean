8d694dac9934810f59286c287e94b1ad85d8a0e4
Sean Evans <sean.w.evans@gmail.com>
2026-08-30 10:02:27 -0400

    add H3 Schwartz preterminal seed bridge

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -609,3 +609,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedSubintervalConsistency
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PreterminalOverlapWitness
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PreterminalSpectralSlice
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PreterminalSeedBridge
