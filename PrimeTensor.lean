f33c54dd97735b88477ac2483e8b632202e60259
Sean Evans <sean.w.evans@gmail.com>
2026-08-29 21:12:43 -0400

    add H3 Schwartz selected velocity third jet continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -600,3 +600,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.CubicPoint3FrechetContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.CubicThirdPartialDifferenceContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.CubicThirdJetContinuity
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedVelocityThirdJetContinuity
