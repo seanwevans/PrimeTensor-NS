f6e8d3f4c3d5b394346f808a9927faae483718c3
Sean Evans <sean.w.evans@gmail.com>
2026-08-30 22:48:08 -0400

    prove selected H3 velocity time continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -643,3 +643,4 @@ import PrimeTensor.Tensor.Calculus
 import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.C1PointEvaluationBound
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedVelocityTimeContinuity
