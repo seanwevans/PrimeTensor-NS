9c5f8f40c2ab117d228c0203155faa15e384a38d
Sean Evans <sean.w.evans@gmail.com>
2026-09-01 15:25:29 -0400

    upgrade selected H3 Duhamel right derivative to ordinary derivative

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -704,3 +704,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelHessianTraceTimeContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedForcingC0TimeContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelDerivativeCandidateTimeContinuity
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelOrdinaryTimeDerivative
