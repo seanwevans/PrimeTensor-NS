4150180a5b0169887e703e9895c4dc666d01624a
Sean Evans <sean.w.evans@gmail.com>
2026-09-01 14:49:53 -0400

    close selected H3 forcing C0 time continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -702,3 +702,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelHessianTraceMild
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Heat.Time.HessianTraceRepresentative
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelHessianTraceTimeContinuity
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedForcingC0TimeContinuity
