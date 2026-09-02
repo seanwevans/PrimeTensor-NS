c51f861be2c9fd7b76888cb2a914a503d4175955
Sean Evans <sean.w.evans@gmail.com>
2026-09-02 13:33:04 -0400

    identify selected H3 first Frechet old-history physical quotient

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -749,3 +749,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelRawThirdMoment
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelFirstFrechetHistoryQuotient
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.H3.Real.C1.FDerivCoordinate
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelFirstFrechetHistoryRepresentativeQuotient
