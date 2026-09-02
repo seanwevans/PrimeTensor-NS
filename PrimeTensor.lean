573d70d3158daedb769a204af5f0fa8749879997
Sean Evans <sean.w.evans@gmail.com>
2026-09-02 09:53:09 -0400

    identify selected H3 Duhamel third Frechet mild form

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -738,3 +738,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedVelocitySpatialTemporalDerivativePDEForm
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedVelocitySpatialTemporalDerivativeTimeContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelFirstFrechetMild
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelThirdFrechetMild
