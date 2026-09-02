18b61ca68049922a9bb1b027293c599574d565af
Sean Evans <sean.w.evans@gmail.com>
2026-09-01 21:51:15 -0400

    prove selected H3 directional forcing gradient time continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -727,3 +727,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedForcingGradientTimeContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedForcingPoint3GradientTimeContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedForcingRealPoint3GradientTimeContinuity
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedForcingDirectionalGradientTimeContinuity
