279b131ec2ee690a634948f25af0dded771edac0
Sean Evans <sean.w.evans@gmail.com>
2026-09-02 12:06:44 -0400

    prove selected H3 Duhamel first Frechet time candidate continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -743,3 +743,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Heat.Time.ThirdCoordinateRepresentative
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Heat.Time.ThirdCoordinateContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelThirdFrechetTimeContinuity
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelFirstFrechetTimeCandidateContinuity
