10d87dc8dace05562124ea158bc3144063ffcdac
Sean Evans <sean.w.evans@gmail.com>
2026-09-02 09:59:44 -0400

    prove selected H3 third Frechet time continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -739,3 +739,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedVelocitySpatialTemporalDerivativeTimeContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelFirstFrechetMild
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedDuhamelThirdFrechetMild
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.CubicThirdFrechetTimeContinuity
