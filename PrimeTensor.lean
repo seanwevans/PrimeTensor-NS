f48b11468ebc7aa4e05be4875b662a7d8ca201ff
Sean Evans <sean.w.evans@gmail.com>
2026-09-03 15:46:18 -0400

    translate selected H3 pressure momentum to absolute time

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -793,3 +793,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedPressureMomentum
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Nonlinear.Forcing.Pressure.SelectedC2
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedPressureAbsoluteTime
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedPressureMomentumAbsoluteTime
