bfeded5039c8c0d6b0e4ad6f29147ae14f62b1b6
Sean Evans <sean.w.evans@gmail.com>
2026-09-03 13:19:37 -0400

    rewrite H3 pressure as bounded Leray rank-one sum

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -786,3 +786,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.RawOuterDivergenceAdvectionBridge
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedRawOuterDivergenceAdvection
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Nonlinear.Forcing.Pressure.Multiplier
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Nonlinear.Forcing.Pressure.Bounded
