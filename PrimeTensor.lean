e2004a74874bc202d2f1db8cd1539e8974c0a0de
Sean Evans <sean.w.evans@gmail.com>
2026-09-03 13:01:53 -0400

    identify selected H3 raw outer divergence with advection

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -784,3 +784,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.H3.Real.C1.Product
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.H3.Real.C1.Realizable
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.RawOuterDivergenceAdvectionBridge
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedRawOuterDivergenceAdvection
