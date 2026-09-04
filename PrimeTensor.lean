f256e5c0f3257b68e728285e60179620fab27198
Sean Evans <sean.w.evans@gmail.com>
2026-09-04 14:49:18 -0400

    package selected H3 canonical first Frechet temporal derivative regularity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -831,3 +831,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalFirstFrechetTimeDerivative
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalFirstFrechetTimeDerivativeCandidateContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalFirstFrechetDerivTimeContinuity
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalFirstFrechetTemporalDerivativeRegularity
