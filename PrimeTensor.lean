b9aafa2cc4cdfd2e3f4e32eb611411726ce6befb
Sean Evans <sean.w.evans@gmail.com>
2026-09-04 14:42:42 -0400

    prove selected H3 canonical first Frechet time derivative candidate continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -829,3 +829,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalC1Representative
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalClassicalMild
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalFirstFrechetTimeDerivative
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalFirstFrechetTimeDerivativeCandidateContinuity
