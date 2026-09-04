6283d430b21f28dee17b4b6bbba2ff530cc1db4c
Sean Evans <sean.w.evans@gmail.com>
2026-09-04 11:23:28 -0400

    integrate H3 Duhamel raw Fourier product

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -816,3 +816,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SpectralFinHeatLerayDuhamelRawFourierPairing
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SpectralFinHeatLerayDuhamelRawFourierJointMeasurable
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SpectralFinHeatLerayDuhamelRawFourierLocalSlice
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SpectralFinHeatLerayDuhamelRawFourierLocalProduct
