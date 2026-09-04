c0bb7383892a0ec3dab9511161435acca7c019d9
Sean Evans <sean.w.evans@gmail.com>
2026-09-04 12:56:29 -0400

    close selected H3 variation of constants state

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -823,3 +823,4 @@ import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedVariationOfConstantsStateReduction
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedVariationOfConstantsWeightedForcing
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedVariationOfConstantsStateClosure
