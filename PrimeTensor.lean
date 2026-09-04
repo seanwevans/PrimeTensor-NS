5ba99187d98f3d7f73f3a734485c7a5c7b37d77b
Sean Evans <sean.w.evans@gmail.com>
2026-09-04 15:39:17 -0400

    package selected H3 canonical real mixed derivative

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -836,3 +836,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalRealSpatialPartialBridge
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalRealMixedDerivativeCandidateBridge
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalRealSpatialPartialTimeDerivative
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.SelectedCanonicalRealMixedDerivative
