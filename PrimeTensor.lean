75e138eb91a54ec05b952a6001d727326a074c89
Sean Evans <sean.w.evans@gmail.com>
2026-09-04 20:11:33 -0400

    identify physical tail endpoint old diffusion

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -855,3 +855,4 @@ import PrimeTensor.Tensor.Calculus
 import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailEndpointCanonicalPressureForce
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailEndpointCanonicalOldDiffusion
