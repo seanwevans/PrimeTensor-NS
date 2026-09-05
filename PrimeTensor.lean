7d1f1dc0ef9e334d08618c76f20cb64e997d2e05
Sean Evans <sean.w.evans@gmail.com>
2026-09-05 11:34:09 -0400

    prove physical tail endpoint Fourier L2 diffusion continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -874,3 +874,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailEndpointCanonicalWeakVelocityPairingFTCReduction
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailEndpointCanonicalWeakL2RHSPairing
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailEndpointCanonicalWeakL2Evolution
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.PhysicalTailEndpointCanonicalFourierL2DiffusionContinuity
