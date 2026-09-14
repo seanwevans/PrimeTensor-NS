5701e94b0df5ea8626c52fcea7e31a2a7b141479
Sean Evans <sean.w.evans@gmail.com>
2026-09-14 19:08:31 -0400

    select late restart after H3 energy regularization

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1055,3 +1055,4 @@ import PrimeTensor.Tensor.Calculus
 import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Physical.Tail.Unit.Viscosity.Frontier.Endpoint.Continuity.Third.EnergyTailRestriction
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Physical.Tail.Unit.Viscosity.Frontier.Endpoint.Continuity.Third.EnergyLateRestart
