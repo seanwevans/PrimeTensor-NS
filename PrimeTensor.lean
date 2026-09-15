d72e0588ba5695a344c87d8aa3d4392e1d6273d8
Sean Evans <sean.w.evans@gmail.com>
2026-09-15 11:21:04 -0400

    derive scalar H3 energy continuity from physical L2 jet continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1090,3 +1090,4 @@ import PrimeTensor.Fluid.Vorticity.H3.Energy.Derivative.OrderTwo
 import PrimeTensor.Fluid.Vorticity.H3.Energy.Derivative.OrderThree
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Physical.Tail.Unit.Viscosity.Frontier.Endpoint.Continuity.Third.EnergyDerivativeAssembly
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Physical.Tail.Unit.Viscosity.Frontier.Endpoint.Continuity.Third.EnergyDerivativeFrontierSplit
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Physical.Tail.Unit.Viscosity.Frontier.Endpoint.Continuity.Third.EnergyFromL2Jet
