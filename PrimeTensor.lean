82107ba998a7110ae4c6a5177fa5fc7a4db50062
Sean Evans <sean.w.evans@gmail.com>
2026-09-18 13:31:09 -0400

    specialize BKM curl multiplier bounds to canonical H3 slice

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1284,3 +1284,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Landau.GradientEnvelope
 import PrimeTensor.Fluid.Vorticity.BKM.Endpoint.FourierAlgebra
 import PrimeTensor.Fluid.Vorticity.BKM.Endpoint.FourierMultiplier
 import PrimeTensor.Fluid.Vorticity.BKM.Endpoint.FourierBound
+import PrimeTensor.Fluid.Vorticity.BKM.Endpoint.CanonicalSlice
