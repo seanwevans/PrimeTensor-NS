16c1208bee9f02a6216a246ce7cb233359119c2e
Sean Evans <sean.w.evans@gmail.com>
2026-09-18 23:13:23 -0400

    identify BKM dyadic convolution Fourier factors

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1309,3 +1309,4 @@ import PrimeTensor.Fluid.Vorticity.BKM.Endpoint.CanonicalVorticityFourier
 import PrimeTensor.Fluid.Vorticity.BKM.Endpoint.CanonicalCurlIntegrable
 import PrimeTensor.Fluid.Vorticity.BKM.Endpoint.LocalizedCurlReconstruction
 import PrimeTensor.Fluid.Vorticity.BKM.Endpoint.PhysicalVorticityConvolutionL2
+import PrimeTensor.Fluid.Vorticity.BKM.Endpoint.DyadicFourierFactors
