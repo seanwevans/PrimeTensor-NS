ea2443d215a155682312fd8488ce604299b58f9d
Sean Evans <sean.w.evans@gmail.com>
2026-09-19 18:56:58 -0400

    derive energy class from preterminal H3 path

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1371,3 +1371,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Selected.HighOrder.Old.LocalPersistence
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Selected.HighOrder.Old.LocalPersistenceContinuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Selected.HighOrder.Old.LocalPersistenceEnergyDerivative
+import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Selected.HighOrder.Old.H3PathAdmissible
