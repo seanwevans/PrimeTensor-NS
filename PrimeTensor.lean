4e5f0514f3e8fce2117ece3aac4808aa964cbd9e
Sean Evans <sean.w.evans@gmail.com>
2026-09-19 20:02:48 -0400

    lower H3 path energy frontier to pointwise data

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1380,3 +1380,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathDirectRestart
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathReducedAnalysis
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathAnalyticOnly
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathAnalyticFrontierSplit
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathPointwiseEnergyFrontier
