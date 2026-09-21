85772472942a29e3ad30ded6e1beb78bd34040df
Sean Evans <sean.w.evans@gmail.com>
2026-09-21 19:04:30 -0400

    reduce selected high Frechet L2 frontier to complex coordinates

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1491,3 +1491,4 @@ import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedHighSpatialJetL2Frontier
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedHighFrechetL2Frontier
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedHighFrechetComplexL2Frontier
