a02a729626e9b781bdde5ac7686888a1da531b47
Sean Evans <sean.w.evans@gmail.com>
2026-09-21 15:01:39 -0400

    remove mixed commutation from H3 BKM frontier

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1477,3 +1477,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Selected.Velocity.Real.Third.Partial.Time.Derivative
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Selected.Velocity.Real.Third.Mixed.Derivative.Absolute.Time
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrder3SelectedClosure
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathMixedCommutationClosure
