ac85a8edd03b6916f4f784635d636b5817916fd9
Sean Evans <sean.w.evans@gmail.com>
2026-09-21 16:49:38 -0400

    split H3 majorants into derivative and temporal pairing frontiers

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1482,3 +1482,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathMajorantFrontier
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathPathMajorantsDiffusionPressureClosure
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathPathMajorantsScalarEnergyClosure
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathPathMajorantsFullScalarClosure
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathTemporalPairingFrontier
