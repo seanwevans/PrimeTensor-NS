78bd693b3cb05f9f4760b7021382acb4ff4ef1d2
Sean Evans <sean.w.evans@gmail.com>
2026-09-21 07:30:20 -0400

    reduce H3 path order three mixed commutation to selected restart

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1437,3 +1437,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Sp
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Selected.Velocity.Real.Second.Mixed.Derivative
 import PrimeTensor.Fluid.Vorticity.Continuation.Restart.Mild.Sobolev.Schwartz.Spectral.Classicalization.Selected.Velocity.Real.Second.Mixed.Derivative.Absolute.Time
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrder2SelectedClosure
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrder3SelectedReduction
