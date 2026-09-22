9b47b31d3d0ffce8d3b07fa8486329c5e1a616f0
Sean Evans <sean.w.evans@gmail.com>
2026-09-22 11:50:52 -0400

    remove selected Duhamel tail L2 from H3 BKM frontier

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1508,3 +1508,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedForcingRadi
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedForcingRadialL2Continuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedDuhamelTailWeightedKernelL2Closure
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedDuhamelTailRadialL2Closure
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedDuhamelTailRadialL2BKMClosure
