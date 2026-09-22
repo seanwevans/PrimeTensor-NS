d77b24ed453151614f0a8e69311f109041e9ce88
Sean Evans <sean.w.evans@gmail.com>
2026-09-22 14:19:00 -0400

    close selected pressure first differentiated L2

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1520,3 +1520,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedPressurePhy
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathPressureL2OrderZero
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedRawForcingRadialL2
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedRawForcingPhysicalL2First
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedPressurePhysicalL2First
