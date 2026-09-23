7f3b27dd2ed53dc51f222e3b3bda0089430eb0f4
Sean Evans <sean.w.evans@gmail.com>
2026-09-23 08:48:18 -0400

    reduce first H3 energy derivative to selected restart

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1547,3 +1547,4 @@ import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedVelocityOld
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOldPhysicalL2PairingBridge
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedOrderZeroCoefficientBridge
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrderZeroEnergyDerivativeClosure
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrderOneSelectedEnergyReduction
