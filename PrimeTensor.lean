99fb77c095cb3c6665846d1d234435a9c105587b
Sean Evans <sean.w.evans@gmail.com>
2026-09-23 17:28:02 -0400

    extend selected forcing radial L2 continuity across restart interval

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1578,3 +1578,4 @@ import PrimeTensor.Tensor.Laws
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrderTwoSelectedVelocityRadialL2Difference
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrderTwoSelectedVelocityRawFourierL2Continuity
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrderTwoSelectedVelocityHeatRadialL2Bound
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathSelectedForcingRadialOpenL2Continuity
