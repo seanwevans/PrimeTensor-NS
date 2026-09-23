f4a807577c953d745e7a96a9092d5d32c9a88c26
Sean Evans <sean.w.evans@gmail.com>
2026-09-23 17:13:47 -0400

    prove selected raw velocity Fourier L2 continuity

--- PrimeTensor.lean
+++ PrimeTensor.lean
@@ -1576,3 +1576,4 @@ import PrimeTensor.Tensor.Calculus
 import PrimeTensor.Tensor.Contract
 import PrimeTensor.Tensor.Laws
 import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrderTwoSelectedVelocityRadialL2Difference
+import PrimeTensor.Fluid.Vorticity.Continuation.Landau.H3PathOrderTwoSelectedVelocityRawFourierL2Continuity
